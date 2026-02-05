be890471cea5c373f6874aae93eef0ce3190d2f1
Klaus-Vensety <imanbay15@gmail.com>
2026-02-05 16:01:21 +0500

    Refactor WeekDay class with new slot management methods
    Updated the WeekDay class to improve functionality and fix issues with slot management. Added methods for checking if slots are empty, finding slots by title, and counting subjects.

--- src/WeekDay.java
+++ src/WeekDay.java
@@ -3,13 +3,13 @@ public class WeekDay {
     private String[] slotList = new String[7];
 
     public enum DAYS {
-        SUNDAY ("Воскресение"),
-        MONDAY ("Понедельник"),
-        TUESDAY ("Вторник"),
-        WEDNESDAY ("Среда"),
-        THURSDAY ("Четверг"),
-        FRIDAY ("Пятница"),
-        SATURDAY("Суббота");
+        MONDAY("Понедельник"),
+        TUESDAY("Вторник"),
+        WEDNESDAY("Среда"),
+        THURSDAY("Четверг"),
+        FRIDAY("Пятница"),
+        SATURDAY("Суббота"),
+        SUNDAY("Воскресенье");
 
         public String title;
 
@@ -23,10 +23,10 @@ public class WeekDay {
     }
 
     public WeekDay(int dayNumber) throws Exception {
-        if(dayNumber > 6 || dayNumber < 1) {
-            throw(new Exception("Некорректный день недели!"));
+        if (dayNumber > 7 || dayNumber < 1) {
+            throw new Exception("Некорректный день недели!");
         }
-        this.name = DAYS.values()[dayNumber].getTitle();
+        this.name = DAYS.values()[dayNumber - 1].getTitle();
     }
 
     public WeekDay(DAYS day) {
@@ -34,52 +34,62 @@ public class WeekDay {
     }
 
     public void add(int slotNumber, String title) throws Exception {
-        if(slotNumber < 0 || slotNumber >= slotList.length) {
-            throw new Exception("Некорректный номер слота! Должен быть 0-6");
+        if (slotNumber < 0 || slotNumber >= slotList.length) {
+            throw new Exception("Некорректный номер слота! Допустимые значения: 0-" + (slotList.length - 1));
         }
-
-        // Проверка: может слот уже занят?
-        if(slotList[slotNumber] != null) {
-            throw new Exception("Этот слот уже занят предметом: " + slotList[slotNumber]);
-        }
-
         slotList[slotNumber] = title;
     }
 
-    // НОВЫЙ МЕТОД: удаление из слота
     public void remove(int slotNumber) throws Exception {
-        if(slotNumber < 0 || slotNumber >= slotList.length) {
-            throw new Exception("Некорректный номер слота! Должен быть 0-6");
+        if (slotNumber < 0 || slotNumber >= slotList.length) {
+            throw new Exception("Некорректный номер слота! Допустимые значения: 0-" + (slotList.length - 1));
         }
-
-        if(slotList[slotNumber] == null) {
-            throw new Exception("В этом слоте нет пары!");
+        if (slotList[slotNumber] == null) {
+            throw new Exception("Слот уже пуст!");
         }
-
         slotList[slotNumber] = null;
     }
 
-    // НОВЫЙ МЕТОД: проверка наличия пары
-    public boolean hasLesson(int slotNumber) {
-        if(slotNumber < 0 || slotNumber >= slotList.length) {
-            return false;
-        }
-        return slotList[slotNumber] != null;
-    }
-
     public void print() {
-        System.out.println("================="+ name +"===============");
-        boolean hasLessons = false;
-        for(int i=0; i < slotList.length; i++) {
-            if(slotList[i] != null) {
+        System.out.println("=================" + name + "===============");
+        for (int i = 0; i < slotList.length; i++) {
+            if (slotList[i] != null) {
                 System.out.print(i + ") ");
                 System.out.println(slotList[i]);
-                hasLessons = true;
             }
         }
-        if(!hasLessons) {
-            System.out.println("   (нет пар)");
-        }
         System.out.println("==========================================");
+    }
+
+    public boolean isEmpty() {
+        for (String subject : slotList) {
+            if (subject != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int findSlotByTitle(String title) {
+        for (int i = 0; i < slotList.length; i++) {
+            if (slotList[i] != null && slotList[i].equalsIgnoreCase(title)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getSubjectCount() {
+        int count = 0;
+        for (String subject : slotList) {
+            if (subject != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String getName() {
+        return name;
     }
 }
